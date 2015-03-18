31eab8e4cd3717f08e5014ddafc3f62faa36dc61
light6899 <lololol@gmail.it>
2015-03-18 19:59:40 +0100

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -1338,6 +1338,23 @@ var commands = exports.commands = {
 		this.sendReplyBox(buffer);
 	},
 
+stafflist: function (target, room, user, connection) {
+var rankLists = {};
+for (var userid in Users.usergroups) {
+var group = Users.usergroups[userid].substr(0, 1);
+if (!rankLists[group]) rankLists[group] = [];
+rankLists[group].push(userid);
+}
+var buffer = [];
+Object.keys(rankLists).sort(function (a, b) {
+return Config.groups[b].rank - Config.groups[a].rank;
+}).forEach(function (r) {
+buffer.push(Config.groups[r].name + "s (" + r + "):\n" + rankLists[r].sort().join(", "));
+});
+if (!buffer.length) return this.sendReply("This server has no staff.");
+connection.popup(buffer.join("\n\n"));
+},
+
 	analysis: 'smogdex',
 	strategy: 'smogdex',
 	smogdex: function (target, room, user) {
