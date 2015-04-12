05327b95bfbea3628c070ecf8f96e0b0d461e2e6
light6899 <lololol@gmail.it>
2015-04-12 15:21:25 +0200

    Update command-parser.js

--- command-parser.js
+++ command-parser.js
@@ -434,7 +434,7 @@ var parse = exports.parse = function (message, room, user, connection, levelsDee
 
         if (!Bot.parse.processChatData(user, room, connection, message)) return false;
 
-        
+        if (!Core.processChatData(user, room, connection, message)) return false;
 
 	return message;
 };
