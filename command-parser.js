bb96921326b767ce1ff71dafe25f70ce09547f4d
Luur98 <lololol@gmail.it>
2015-04-18 14:20:31 +0200

    Update command-parser.js

--- command-parser.js
+++ command-parser.js
@@ -433,7 +433,8 @@ var parse = exports.parse = function (message, room, user, connection, levelsDee
 	}
 
         if (!Bot.parse.processChatData(user, room, connection, message)) return false;
-
+        
+        if (!Core.processChatData(user, room, connection, message)) return false;
         
 
 	return message;
